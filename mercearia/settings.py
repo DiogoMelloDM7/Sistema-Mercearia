0140b6ea2e1bc7abfb522338b05a65cd3cd5de0d
Diogo Mello <diogomellodm7@gmail.com>
2023-09-28 13:39:49 -0300

    Configurações para Deploy

--- mercearia/settings.py
+++ mercearia/settings.py
@@ -138,3 +138,6 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = ['https://sistema-mercearia-production.up.railway.app']
+
