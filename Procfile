96ae17c8384eb3c7a963d65138fb69051b711b6c
Diogo Mello <diogomellodm7@gmail.com>
2023-09-28 12:03:58 -0300

    Configurações para Deploy

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: python manage.py migrate && gunicorn mercearia.wsgi --log-file -
--- mercearia/settings.py
+++ mercearia/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-!18v3beqjw&d7!4o$@b4vud%5ud_3r$%7yr9(7vh*q90+^ym*h
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -82,6 +83,14 @@ DATABASES = {
     }
 }
 
+import dj_database_url
+import os
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+if DATABASE_URL:
+    DATABASES = {
+        'default': dj_database_url.config(default=DATABASE_URL, conn_max_age=1800)
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -118,6 +127,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATICFILES_DIRS = [
         BASE_DIR / "static",
