30f4ea16368d2ffb27a1d9dcae76f6422bbb3565
Diogo Mello <diogomellodm7@gmail.com>
2024-10-10 19:53:31 -0300

    alterações no formulário

--- static/js/clientes.js
+++ static/js/clientes.js
@@ -3,24 +3,28 @@ function exibeCadastro() {
     $("#formEditar").hide();
     $("#tabelaDeClientes").hide();
     $("#formCadastro").show();
+    resetaFormEditar();
 }
 
 function exibeEditarCadastro() {
-    
+    constroiTabelaClientes();
     $("#formCadastro").hide();
     $("#formEditar").show();
-    $("#tabelaDeClientes").show();
+    $("#tabelaDeClientes").hide();
+    console.log(clientes)
 }
 
 function exibeCliente() {
-
+    constroiTabela();
     $("#formCadastro").hide();
     $("#formEditar").hide();
     $("#tabelaDeClientes").show();
+    resetaFormEditar();
 }
 
 function constroiTabela(){
-    $("#tabelaClientes thead").append(`
+
+    $("#tabelaClientes thead").empty().append(`
         <tr>
             <th>Cliente</th>
             <th>Rua</th>
@@ -32,6 +36,7 @@ function constroiTabela(){
         </tr>
     `)
 
+    $("#tabelaClientes tbody").empty();
     clientes.forEach(function(cliente, index){
         $("#tabelaClientes tbody").append(`
             <tr>
@@ -47,7 +52,133 @@ function constroiTabela(){
     })
 }
 
-$(document).ready(function(){
-    console.log(clientes,'js');
-    constroiTabela();
-})  
+function constroiTabelaClientes(){
+    $("#tabelaEdicaoClientes thead").empty().append(`
+        <tr>
+            <th>Cliente</th>
+            <th>Rua</th>
+            <th>Bairro</th>
+            <th>Cidade</th>
+            <th>CPF</th>
+            <th>Telefone</th>
+            <th>E-mail</th>
+            <th>Ações</th>
+        </tr>
+    `)
+
+    $("#tabelaEdicaoClientes tbody").empty();
+    clientes.forEach(function(cliente, index){
+        $("#tabelaEdicaoClientes tbody").append(`
+            <tr>
+            <td>${cliente.nome}</td>
+            <td>${cliente.rua}</td>
+            <td>${cliente.bairro}</td>
+            <td>${cliente.cidade}</td>
+            <td>${cliente.cpf}</td>
+            <td>${cliente.telefone}</td>
+            <td>${cliente.email}</td>
+            <td>
+                <button class="btn btn-sm btn-primary edit-cliente-btn" data-index=${index}>Editar</button>
+            </td> 
+        </tr>
+`)
+    })
+}
+
+$(document).ready(function () {
+    
+
+
+    $("#clienteNome").on('keyup', function () { 
+        let clienteNome = $("#clienteNome").val().toLowerCase();
+
+        $("#tabelaClientes thead").empty().append(`
+            <tr>
+                <th>Cliente</th>
+                <th>Rua</th>
+                <th>Bairro</th>
+                <th>Cidade</th>
+                <th>CPF</th>
+                <th>Telefone</th>
+                <th>E-mail</th>
+            </tr>
+        `);
+
+        $("#tabelaClientes tbody").empty();
+        clientes.forEach(function (cliente, index) {
+            if (cliente.nome.toLowerCase().includes(clienteNome)) {
+                $("#tabelaClientes tbody").append(`
+                    <tr>
+                        <td>${cliente.nome}</td>
+                        <td>${cliente.rua}</td>
+                        <td>${cliente.bairro}</td>
+                        <td>${cliente.cidade}</td>
+                        <td>${cliente.cpf}</td>
+                        <td>${cliente.telefone}</td>
+                        <td>${cliente.email}</td> 
+                    </tr>
+                `);
+            }
+        });
+    });
+
+    $("#clienteEditarNome").on('keyup', function () { 
+        let clienteNomeEditar = $("#clienteEditarNome").val().toLowerCase();
+
+        $("#tabelaEdicaoClientes thead").empty().append(`
+            <tr>
+                <th>Cliente</th>
+                <th>Rua</th>
+                <th>Bairro</th>
+                <th>Cidade</th>
+                <th>CPF</th>
+                <th>Telefone</th>
+                <th>E-mail</th>
+            </tr>
+        `);
+
+        $("#tabelaEdicaoClientes tbody").empty();
+        clientes.forEach(function (cliente, index) {
+            if (cliente.nome.toLowerCase().includes(clienteNomeEditar)) {
+                $("#tabelaEdicaoClientes tbody").append(`
+                    <tr>
+                        <td>${cliente.nome}</td>
+                        <td>${cliente.rua}</td>
+                        <td>${cliente.bairro}</td>
+                        <td>${cliente.cidade}</td>
+                        <td>${cliente.cpf}</td>
+                        <td>${cliente.telefone}</td>
+                        <td>${cliente.email}</td> 
+                        <td>
+                            <button class="btn btn-sm btn-primary edit-cliente-btn" data-index=${index}>Editar</button>
+                        </td>
+                    </tr>
+                `);
+            }
+        });
+    });
+
+    $(document).on("click", '.edit-cliente-btn', function(){
+
+        let indexVenda = $(this).data('index');
+
+        $("#nomeEdit").val(clientes[indexVenda].nome);
+        $("#ruaEdit").val(clientes[indexVenda].rua);
+        $("#bairroEdit").val(clientes[indexVenda].bairro);
+        $("#cidadeEdit").val(clientes[indexVenda].cidade);
+        $("#cpfEdit").val(clientes[indexVenda].cpf);
+        $("#telefoneEdit").val(clientes[indexVenda].telefone);
+        $("#emailEdit").val(clientes[indexVenda].email);
+    })
+
+});
+
+function resetaFormEditar(){
+    $("#nomeEdit").val('');
+        $("#ruaEdit").val('');
+        $("#bairroEdit").val('');
+        $("#cidadeEdit").val('');
+        $("#cpfEdit").val('');
+        $("#telefoneEdit").val('');
+        $("#emailEdit").val('');
+}
